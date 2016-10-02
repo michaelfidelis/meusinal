fa8abf90368b96d05752417f949293a0af7e9bf5
michaelfidelis <mclexr@gmail.com>
2016-10-02 05:38:02 -0300

    Tuplas!

--- Dicionario.py
+++ Dicionario.py
@@ -8,14 +8,15 @@ class Dicionario:
     def __init__(self, fila):
         self.dicionario = {}
         self.fila = fila
-        self.dicionario['J'] = ['I', 'IX']
+        self.dicionario['J'] = (['I', 'IX'], ['J', 'IX'])
 
     def verifica(self):
-        for chave, valor in self.dicionario.iteritems():
-            if (len(self.fila) >= len(valor)):
-                sequencia = list(itertools.islice(self.fila, len(self.fila) - len(valor), len(self.fila)))
-                if (sequencia == valor):
-                    for x in range(0, len(valor)):
-                        self.fila.pop()
-                    self.fila.append(chave)
+        for chave, tupla in self.dicionario.iteritems():
+            for valor in tupla:
+                if (len(self.fila) >= len(valor)):
+                    sequencia = list(itertools.islice(self.fila, len(self.fila) - len(valor), len(self.fila)))
+                    if (sequencia == valor):
+                        for x in range(0, len(valor)):
+                            self.fila.pop()
+                        self.fila.append(chave)
 
